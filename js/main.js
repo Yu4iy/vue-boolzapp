2c84dcab62fea2781d281bfb5a33fe4f25281414
Yu4iy <yurimohyliuk@gmail.com>
2021-11-17 12:58:10 +0100

    add  logic frst part

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const root = new Vue({
 		userProfile:{
 			name: 'Paolo',
 			surName:'Duzioni',
-			avatar: 'img/avatar_io.jpg',
+			avatar: '_io',
 		},
 		contacts: [
 			{
 				 name: 'Michele',
-				 avatar: 'img/avatar_1.jpg',
+				 avatar: '_1',
 				 visible: true,
 				 messages: [
 					  {
@@ -31,7 +31,7 @@ const root = new Vue({
 			},
 			{
 				 name: 'Fabio',
-				 avatar: 'img/avatar_2.jpg',
+				 avatar: '_2',
 				 visible: true,
 				 messages: [
 					  {
@@ -52,7 +52,7 @@ const root = new Vue({
 				 ],
 			},    {
 				 name: 'Samuele',
-				 avatar: 'img/avatar_3.jpg',
+				 avatar: '_3',
 				 visible: true,
 				 messages: [
 					  {
@@ -74,7 +74,7 @@ const root = new Vue({
 			},
 			{
 				 name: 'Luisa',
-				 avatar: 'img/avatar_4.jpg',
+				 avatar: '_4',
 				 visible: true,
 				 messages: [
 					  {
@@ -89,8 +89,15 @@ const root = new Vue({
 					  }
 				 ],
 			},
-	  ]
-	  
+	  ],
+	  avtiveContact:0,
+	  ff:false
+	},
+	methods:{
+		showMessage(index){
+			this.avtiveContact = index
+			return
+		}
 	}
 	
 	
